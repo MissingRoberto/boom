6e3377e9ab3d39b60a7376af55a35d34696d1a5d
Roberto Jimenez Sanchez <roberto@de.ibm.com>
2017-02-04 01:45:19 +0000

    Update resource pool size

--- boom/boom.go
+++ boom/boom.go
@@ -10,12 +10,19 @@ type Boom struct {
 }
 
 type Manifest struct {
-	Jobs []Job `yaml:"jobs"`
+	Jobs          []Job          `yaml:"jobs"`
+	ResourcePools []ResourcePool `yaml:"resource_pools"`
 }
 
 type Job struct {
-	Name      string
-	Instances int
+	Name         string `yaml:"name"`
+	Instances    int    `yaml:"instances"`
+	ResourcePool string `yaml:"resource_pool"`
+}
+
+type ResourcePool struct {
+	Name string `yaml:"name"`
+	Size int    `yaml:"size"`
 }
 
 func (b *Boom) ScaleInstances(manifest *Manifest, name string, factor float32) error {
@@ -37,6 +44,10 @@ func (b *Boom) ScaleInstances(manifest *Manifest, name string, factor float32) e
 		}
 	}
 
+	indexResourcePool, err := findResourcePool(manifest, manifest.Jobs[index].ResourcePool)
+	if err == nil {
+		manifest.ResourcePools[indexResourcePool].Size = newValue
+	}
 	manifest.Jobs[index].Instances = newValue
 	return nil
 }
@@ -45,6 +56,11 @@ func (b *Boom) SetInstances(manifest *Manifest, name string, value int) error {
 	index, err := findJob(manifest, name)
 	if err != nil {
 		return err
+	}
+
+	indexResourcePool, err := findResourcePool(manifest, manifest.Jobs[index].ResourcePool)
+	if err == nil {
+		manifest.ResourcePools[indexResourcePool].Size = value
 	}
 
 	manifest.Jobs[index].Instances = value
@@ -59,5 +75,16 @@ func findJob(manifest *Manifest, name string) (int, error) {
 		}
 	}
 	return -1, errors.New(fmt.Sprintf("job `%v` not found", name))
+
+}
+
+func findResourcePool(manifest *Manifest, name string) (int, error) {
+
+	for index, resourcePool := range manifest.ResourcePools {
+		if name == resourcePool.Name {
+			return index, nil
+		}
+	}
+	return -1, errors.New(fmt.Sprintf("resource_pool `%v` not found", name))
 
 }
