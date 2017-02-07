b2ab3c274829767717da6f4fdbb9bfa29f22ecba
Roberto Jimenez Sanchez <roberto@de.ibm.com>
2017-02-07 08:12:13 +0000

    Update descriptions

--- cli/main.go
+++ cli/main.go
@@ -11,28 +11,31 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.EnableBashCompletion = true
-
+	app.Version = "0.1.0"
+	app.Name = "boom"
+	app.Usage = "a simple and quick tool for bosh manifest maintenance"
 	app.Commands = []cli.Command{
 		{
-			Name:    "set-instances",
-			Aliases: []string{"si"},
-			Usage:   "Sets the number of instances",
-			Action:  setInstances,
+			Name:      "set-instances",
+			Aliases:   []string{"si"},
+			Usage:     "Sets the number of instances",
+			ArgsUsage: "<MANIFEST> <JOB_NAME> <VALUE>\n\nExample:\n $ boom set-instances manifest.yml cell 10",
+			Action:    setInstances,
 			Flags: []cli.Flag{
-				cli.BoolFlag{Name: "output, o"},
-				cli.BoolFlag{Name: "force, f"},
-				cli.BoolFlag{Name: "diff, d"},
+				cli.BoolFlag{Name: "output, o", Usage: "prints result into stdout"},
+				cli.BoolFlag{Name: "diff, d", Usage: "displays differences using wdiff"},
 			},
 		},
 		{
-			Name:    "scale-instances",
-			Aliases: []string{"sc"},
-			Usage:   "Scale number of instances",
-			Action:  scaleInstances,
+			Name:      "scale-instances",
+			Aliases:   []string{"sc"},
+			Usage:     "Scale number of instances",
+			ArgsUsage: "<MANIFEST> <JOB_NAME> <FACTOR>\n\nExample:\n $ boom scale-instances manifest.yml cell 1.5",
+			Action:    scaleInstances,
 			Flags: []cli.Flag{
-				cli.BoolFlag{Name: "output, o"},
-				cli.BoolFlag{Name: "force, f"},
-				cli.BoolFlag{Name: "diff, d"},
+				cli.BoolFlag{Name: "output, o", Usage: "prints result into stdout"},
+				cli.BoolFlag{Name: "diff, d", Usage: "displays differences using wdiff"},
+				cli.BoolFlag{Name: "force, f", Usage: "forces to change the current value at least an unit"},
 			},
 		},
 	}
@@ -44,7 +47,7 @@ func setInstances(c *cli.Context) {
 
 	args := c.Args()
 
-	boom := boomPkg.New(args.First(), c.Bool("force"))
+	boom := boomPkg.New(args.First(), false)
 	size, err := strconv.Atoi(args.Get(2))
 	if err != nil {
 		exitWithError(err)
